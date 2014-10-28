a74a5a59f08ae21b9f8aa5ecd5e2c1914c5e3eaf
Rich <piemonkey@gmail.com>
2014-10-28 07:57:46 +0000

    Add number of sub items to model

--- app/src/main/java/com/placeholder/rich/todotreeprototype/model/Item.java
+++ app/src/main/java/com/placeholder/rich/todotreeprototype/model/Item.java
@@ -3,14 +3,22 @@ package com.placeholder.rich.todotreeprototype.model;
 public class Item {
     private final String name;
     private boolean complete;
+    private int nSubItems;
+    private int nItemsLeft;
 
     public Item(String name) {
-        this(name, false);
+        this(name, false, 0, 0);
     }
 
     public Item(String name, boolean complete) {
+        this(name, complete, 0, 0);
+    }
+
+    public Item(String name, boolean complete, int nSubItems, int nItemsLeft) {
         this.name = name;
         this.complete = complete;
+        this.nSubItems = nSubItems;
+        this.nItemsLeft = nItemsLeft;
     }
 
     public String getName() {
@@ -23,6 +31,18 @@ public class Item {
 
     public boolean toggleComplete() {
         return complete = !complete;
+    }
+
+    public int getNSubItems() {
+        return nSubItems;
+    }
+
+    public int getNItemsLeft() {
+        return nItemsLeft;
+    }
+
+    public boolean hasSubItems() {
+        return nSubItems > 0;
     }
 
 }
