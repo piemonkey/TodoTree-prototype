fa509274632961fa23d6f5d1b9f46fef5c4eab89
Rich <piemonkey@gmail.com>
2015-03-23 22:00:33 +1100

    Add WhenRolloverService to handle moving tomorrow tasks to today

---
+++ app/src/main/java/com/placeholder/rich/todotreeprototype/WhenRolloverService.java
@@ -0,0 +1,42 @@
+package com.placeholder.rich.todotreeprototype;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+import com.placeholder.rich.todotreeprototype.infrastructure.ListStore;
+import com.placeholder.rich.todotreeprototype.infrastructure.ListStoreSQLite;
+import com.placeholder.rich.todotreeprototype.model.Item;
+import com.placeholder.rich.todotreeprototype.model.TagList;
+import com.placeholder.rich.todotreeprototype.model.When;
+
+public class WhenRolloverService extends IntentService {
+    public static final String ACTION_ROLLOVER_DAY =
+            "com.placeholder.rich.todotreeprototype.action.ROLLOVER_DAY";
+
+    private ListStore listStore;
+
+    public WhenRolloverService() {
+        super("WhenRolloverService");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        listStore = new ListStoreSQLite(this);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_ROLLOVER_DAY.equals(action)) {
+                TagList tomorrow = listStore.loadTagged(When.TOMORROW);
+                for (Item item : tomorrow.getItems()) {
+                    item.doToday();
+                    listStore.save(item);
+                }
+            }
+        }
+    }
+
+}
